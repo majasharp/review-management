bd38a68a2795d766d0511b0cf31b9dff7317d7d9
Andrew Williamson <andrewpwilliamson@googlemail.com>
2022-03-23 16:41:11 +0000

    Added code to insert sentiment_scpore values into review_clone table

--- main.py
+++ main.py
@@ -33,11 +33,11 @@ def main ():
 
 
     for x in range (1):
-        random_sentiment = round(random.uniform(-1, 1), 3)
-        SQL = "UPDATE review_clone_test SET sentiment_score = (%s) WHERE id = 1"
-        val = (random_sentiment)
+        random_sentiment = round(random.uniform(-1, 1), 3) #Generate random sentiment score between -1 and 1
+        SQL = "UPDATE review_clone_test SET sentiment_score = (%s) WHERE id = 1" #SQL query to run
+        val = (random_sentiment) #random sentiment score value to be inserted into sql query
 
-        repository.insert_values(SQL, val)
+        repository.insert_values(SQL, val) #Send  sql and SS value to insert_values() in repository.py
         repository.rmsdb.commit()
 
 
